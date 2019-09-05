0fddc41e2903f63948fc38456e6c8372c5f7bd14
Jonathan Luk <jluk87@gmail.com>
2019-09-05 23:19:55 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -132,7 +132,7 @@ function displayGames() {
       if (data['data'].length > 0) {
         var gamesHTML = ''
         for (var i = 0; i < data['data'].length; i++) {
-          gamesHTML += '<button>' + data['data'][i]['id'] + ' - ' + data['data'][i]['attributes']['updated-at'].toLocaleTimeString() + '</button>'
+          gamesHTML += '<button>' + data['data'][i]['id'] + ' - ' + data['data'][i]['attributes']['updated-at'].toDateString() + '</button>'
           console.log(data)
         }
         $('#games').html(gamesHTML)
